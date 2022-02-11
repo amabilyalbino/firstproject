5e4a65e4a8449f2b611ffc633051f390326f9798
Luiz Sutil <luizsutil@Luizs-MacBook-Air.local>
2022-02-10 23:19:30 -0300

    primeiro commit

---
+++ helloworld.js
@@ -0,0 +1,111 @@
+// comentario
+// 2 tipos de logicas importantes
+// - Condicional
+// - Iterativo
+
+// var connections = ["Ramon","Andre","Sophia"]
+// console.log(connections[0])
+// console.log(connections[1])
+// console.log(connections[2])
+
+// var valor = 1
+// for (var i = 5; i < 4; i++) {
+//     console.log(connections[i])
+// }
+
+// var numero = 7 % 2
+// console.log(numero)
+
+// i = 0
+// i < 3 ?
+// imprimir o item na lista com o index 0 => imprimir ramon
+// i = 1
+// i < 3 ?
+// imprimir o item na lista com o index 1 => imprimir andre
+
+
+
+
+
+// if (valor > 1) {
+//     console.log("valor maior que 1")
+// } else if (valor < 1) {
+//     console.log("valor menor que 1")
+// }
+
+// Fazer um programa que imprime os valores de 1 a 10 
+// se o valor for par, imprimir a palavra foo embaixo do numero
+// se o valor for impar, imprimir a palavra bar embaixo do numero
+// usando for e if
+// ex:
+// 1
+// bar
+// 2 
+// foo
+// 3
+// bar
+// 4 
+// foo
+// ajuda: 2 % 2 = 0
+//        5 % 2 = 1
+
+// for (var i = 1; i < 11; i++){
+
+//     console.log(i)
+
+//     if (i % 2  == 1) {
+//         console.log("bar")
+//     } else if (i % 2  == 0) {
+//         console.log("foo")
+//     }
+//     if (i == 10) {
+//         console.log("Hello World!")
+//     }
+// }
+
+
+
+
+
+// funcao log 
+// com parametro qualquer
+// quando usada vai imprimir o parametro no terminal (console)
+
+
+
+// const amabs = "legal" // maczinho, guarda na tua memoria uma constante de nome amabs que deve ter o valor de legal
+
+// maczinho, guarda na tua memoria uma funçao. O nome dela vai ser funcaozinha, e ela vai ter dois parametros.
+// Quando eu usar ela, eu quero que tu imprimia para o console a soma dos dois parametros.
+
+
+// essa funcao pega dois parametros, soma eles e joga pro console.
+// const adicionar = (a,b) => {
+//     console.log(a+b)
+// }
+
+
+// add(data,duraçao)
+
+//condição 
+
+var porta = "queimando"
+
+
+// console.log(porta === "fechada")
+
+// como pergunto pro maczinho se a porta esta aberta?
+if (porta === "aberta") {
+    console.log("Pode entrar!")
+} else if (porta === "fechada") {
+    console.log("bater na porta")
+} else if (porta === "queimando"){
+    console.log("pegar o extintor")
+}
+
+
+
+
+// if(porta === "fechada"){
+//     console.log("bater na porta")
+// }
